afb13f5d4d0e6ad8ad616d23490050445950829e
loops <128599880+loopsc@users.noreply.github.com>
2025-02-09 19:55:51 +1300

    Set max digits to 9.
    Calculator no longer calculates numbers longer than 9 digits.

--- script.js
+++ script.js
@@ -23,8 +23,8 @@ function operate(op, numOne, numTwo) {
         case "*":
             return mul(numOne, numTwo);
         case "/":
-            if (numOne == 0 && numTwo == 0){
-                return 'lol'
+            if (numOne == 0 && numTwo == 0) {
+                return "lol";
             }
             return div(numOne, numTwo);
     }
@@ -34,54 +34,55 @@ const display = document.querySelector("#display");
 let numOne = "";
 let numTwo = "";
 let operator = "";
-let numOfCalculations = 0
+let numOfCalculations = 0;
 
 const numberNodes = document.querySelectorAll(".number").forEach((number) => {
     number.addEventListener("click", () => {
         if (operator == "") {
-            numOne += number.textContent;
-            console.log("numOne", typeof numOne, numOne);
+            if (numOne.length < 9) {
+                numOne += number.textContent;
+                console.log("numOne", typeof numOne, numOne);
 
-            display.textContent = numOne;
-        } else {
-            if (numOne == ''){
-                numOne = '0'
+                display.textContent = numOne;
             }
-            numTwo += number.textContent;
-            console.log("numTwo", typeof numTwo, numTwo);
+        } else {
+            if (numOne == "") {
+                numOne = "0";
+            }
+            if (numTwo.length < 9) {
+                numTwo += number.textContent;
+                console.log("numTwo", typeof numTwo, numTwo);
 
-            display.textContent = numTwo;
+                display.textContent = numTwo;
+            }
         }
     });
 });
 
 const operatorNodes = document.querySelectorAll(".operator").forEach((op) => {
     op.addEventListener("click", () => {
-        
         // If the user clicks any of the mathematical operators
         if (op.textContent != "=" && op.textContent != "C") {
-            numOfCalculations += 1
+            numOfCalculations += 1;
             // If this is the first operation
             // Save the operator and display on the display
             if (numOfCalculations == 1) {
                 operator = op.textContent;
                 console.log("operator", typeof operator, operator);
                 display.textContent = operator;
+            } else {
+                numOne = operate(operator, +numOne, +numTwo);
+                display.textContent = numOne;
+                numTwo = "";
+                operator = op.textContent;
             }
-            else {
-                numOne = operate(operator,+numOne,+numTwo)
-                display.textContent = numOne
-                numTwo = ''
-                operator = op.textContent
-            }
-            
-            
-        // If the user clicks '='
+
+            // If the user clicks '='
         } else if (op.textContent == "=") {
             // Defaults the first number to 0 if nothing is selected
             if (numOne == "") {
                 display.textContent = "0";
-            // Defaults the number to numOne if no operator or numTwo is selected
+                // Defaults the number to numOne if no operator or numTwo is selected
             } else if (operator == "" || numTwo == "") {
                 display.textContent = numOne;
             } else {
@@ -89,7 +90,7 @@ const operatorNodes = document.querySelectorAll(".operator").forEach((op) => {
                 console.log(`${numOne} ${operator} ${numTwo}`);
                 display.textContent = operate(operator, +numOne, +numTwo);
             }
-        // If the user clicks 'C'
+            // If the user clicks 'C'
         } else {
             numOne = "";
             numTwo = "";
