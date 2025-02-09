4ccfb0c0705a194e034af43733a9f0e0ff6a8697
loops <128599880+loopsc@users.noreply.github.com>
2025-02-09 17:58:03 +1300

    Calculator layout and functionality changes.
    Calculator layout and alignment improved. Calculator is flexive.
    Display no shows multiple digits (e.g. 22 instead of 2 and 2) and operators.

--- script.js
+++ script.js
@@ -14,27 +14,48 @@ function div(a, b) {
     return a / b;
 }
 
-function operate(op,numOne,numTwo){
-    switch(op){
-        case '+':
-            console.log(add(numOne,numTwo))
-        case '-':
-            console.log(sub(numOne,numTwo))
-        case '*':
-            console.log(mul(numOne,numTwo))
-        case '/':
-            console.log(div(numOne,numTwo))
+function operate(op, numOne, numTwo) {
+    switch (op) {
+        case "+":
+            console.log(add(numOne, numTwo));
+        case "-":
+            console.log(sub(numOne, numTwo));
+        case "*":
+            console.log(mul(numOne, numTwo));
+        case "/":
+            console.log(div(numOne, numTwo));
     }
 }
 
-const display = document.querySelector('#display')
+const display = document.querySelector("#display");
+let numOne = "";
+let numTwo = "";
+let operator = "";
 
-const numbers = document.querySelectorAll('.number').forEach(number => {
-    number.addEventListener('click',() => {
-        display.textContent = number.textContent
-    })
-})
+const numberNodes = document.querySelectorAll(".number").forEach((number) => {
+    number.addEventListener("click", () => {
+        numOne += number.textContent;
+        numOne = +numOne;
+        console.log(typeof numOne, numOne);
 
-let a = 0
-let b = 0
-let op = '+'
+        display.textContent = numOne;
+    });
+});
+
+const operatorNodes = document.querySelectorAll(".operator").forEach((op) => {
+    op.addEventListener("click", () => {
+        if (op.textContent != "=") {
+            operator = op.textContent;
+            console.log(typeof operator, operator);
+
+            display.textContent = op.textContent;
+        }
+
+        // perform operation
+        console.log("= clicked");
+    });
+});
+
+let a = 0;
+let b = 0;
+let op = "+";
